da134e865a8c987e28556b3771fff31e0a2e99bc
Vitor Cesar <vitor@fasters.com.br>
2023-05-05 10:27:43 -0300

    refactor: stop proccess on function to check numbers

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const rl = readline.createInterface({
 });
 
 const checkNumbers = (value) => {
-  return /^\d+$/.test(value);
+  const isNumber = /^\d+$/.test(value);
+
+  if (!isNumber) {
+    console.log(
+      "Preencha a informação solicitada somente com números! Finalizando..."
+    );
+    process.exit(1);
+  }
 };
 
 const calculateImc = (weight, height) => {
@@ -18,16 +25,9 @@ const calculateImc = (weight, height) => {
 rl.question(
   "Olá, seja bem-vindo para o cálculo de IMC, comece digitando sua altura em centímetros e sem pontuação:\n",
   (height) => {
-    if (checkNumbers(height) === false) {
-      console.log("Preencha sua altura somente com números! Finalizando...\n");
-      rl.close();
-    }
+    checkNumbers(height);
     rl.question("Agora informe o seu peso em quilos..\n", (weight) => {
-      if (checkNumbers(weight) === false) {
-        console.log("Preencha seu peso somente com números! Finalizando...\n");
-        rl.close();
-      }
-
+      checkNumbers(weight);
       const imc = calculateImc(weight, height);
       console.log(`seu imc é ${imc}`);
       rl.close();
